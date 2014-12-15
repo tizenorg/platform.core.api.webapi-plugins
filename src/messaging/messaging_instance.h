0e7210aa623f09c1f3293db18fd4bacd4b70aa6b
Robert Karolak <r.karolak@samsung.com>
2014-12-15 16:26:55 +0100

    [Messaging] Attach methods from javascript to corresponding
    methods from C++
    
    [Verification] Code compiles without error
    
    Change-Id: I9d5cd4a4f8761f09be6051ba1f891e15ef3eef6b
    Signed-off-by: Robert Karolak <r.karolak@samsung.com>

--- src/messaging/messaging_instance.h
+++ src/messaging/messaging_instance.h
@@ -10,15 +10,49 @@
 namespace extension {
 namespace messaging {
 
-class MessagingInstance : public common::ParsedInstance {
-    public:
-        static MessagingInstance& getInstance();
-    private:
-        MessagingInstance();
-        virtual ~MessagingInstance();
+class MessagingInstance: public common::ParsedInstance {
+public:
+    static MessagingInstance& getInstance();
+private:
+    MessagingInstance();
+    virtual ~MessagingInstance();
 
-        void GetMessageServices(const picojson::value& args, picojson::object& out);
-        void MessageServiceSync(const picojson::value& args, picojson::object& out);
+    void GetMessageServices(const picojson::value& args, picojson::object& out);
+
+    void MessageServiceSendMessage(const picojson::value& args,
+            picojson::object& out);
+    void MessageServiceLoadMessageBody(const picojson::value& args,
+            picojson::object& out);
+    void MessageServiceLoadMessageAttachment(const picojson::value& args,
+            picojson::object& out);
+    void MessageServiceSync(const picojson::value& args, picojson::object& out);
+    void MessageServiceSyncFolder(const picojson::value& args,
+            picojson::object& out);
+    void MessageServiceStopSync(const picojson::value& args,
+            picojson::object& out);
+
+    void MessageStorageAddDraft(const picojson::value& args,
+            picojson::object& out);
+    void MessageStorageFindMessages(const picojson::value& args,
+            picojson::object& out);
+    void MessageStorageRemoveMessages(const picojson::value& args,
+            picojson::object& out);
+    void MessageStorageUpdateMessages(const picojson::value& args,
+            picojson::object& out);
+    void MessageStorageFindConversations(const picojson::value& args,
+            picojson::object& out);
+    void MessageStorageRemoveConversations(const picojson::value& args,
+            picojson::object& out);
+    void MessageStorageFindFolders(const picojson::value& args,
+            picojson::object& out);
+    void MessageStorageAddMessagesChangeListener(const picojson::value& args,
+            picojson::object& out);
+    void MessageStorageAddConversationsChangeListener(const picojson::value& args,
+            picojson::object& out);
+    void MessageStorageAddFolderChangeListener(const picojson::value& args,
+            picojson::object& out);
+    void MessageStorageRemoveChangeListener(const picojson::value& args,
+            picojson::object& out);
 };
 
 } // namespace messaging
