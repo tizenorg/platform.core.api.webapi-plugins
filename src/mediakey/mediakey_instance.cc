85d67c46f91f53a487a07d3c1eab2c6ec2dc4b73
Piotr Kosko <p.kosko@samsung.com>
2015-05-27 14:09:52 +0200

    [MediaKey] Added logs in methods onEntered and onReturn
    Change-Id: I5f3721bbd0c7b79e941dc4fbb190476edcfce595
    Signed-off-by: Piotr Kosko <p.kosko@samsung.com>

--- src/mediakey/mediakey_instance.cc
+++ src/mediakey/mediakey_instance.cc
@@ -52,6 +52,9 @@ MediaKeyInstance::~MediaKeyInstance() {
 
 void MediaKeyInstance::SetMediaKeyEventListener(const picojson::value& args,
                                                 picojson::object& out) {
+
+  LoggerD("Enter");
+
   common::PlatformResult result = MediaKeyManager::GetInstance()
       .RegisterMediaKeyEventListener(this);
   if (result.IsError()) {
@@ -64,6 +67,7 @@ void MediaKeyInstance::SetMediaKeyEventListener(const picojson::value& args,
 
 void MediaKeyInstance::UnsetMediaKeyEventListener(const picojson::value& args,
                                                   picojson::object& out) {
+  LoggerD("Enter");
   common::PlatformResult result = MediaKeyManager::GetInstance()
       .UnregisterMediaKeyEventListener();
   if (result.IsError()) {
