b23cbe732fd80ce53b6d03c1c2e144d6368e3a0d
Dongyoung Kim <dydot1.kim@samsung.com>
2015-05-29 15:52:52 +0900

    [WebDeviceAPI] modify the boilerplate of source code.
    Change-Id: I69e3bd1e2f7e0521b299a8bcdaa633c48fff0c9c

--- src/common/dbus_operation.cc
+++ src/common/dbus_operation.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-
+ 
 #include "dbus_operation.h"
 
 #include <string>
