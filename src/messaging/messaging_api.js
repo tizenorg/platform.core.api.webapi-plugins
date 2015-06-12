1299833a63332aa4b7aa1b1289db0c9b95f81830
Pawel Andruszkiewicz <p.andruszkie@samsung.com>
2015-06-12 10:55:28 +0200

    [MessagingEmail] Fixed argument check in stopSync().
    [Verification] Following TCT should pass:
     MessageService_email_stopSync_missarg
     MessageService_email_stopSync_opId_invalid
    
    Change-Id: If3e8f6296cf98543a449197a11d167836875abff
    Signed-off-by: Pawel Andruszkiewicz <p.andruszkie@samsung.com>

--- src/messaging/messaging_api.js
+++ src/messaging/messaging_api.js
@@ -938,11 +938,6 @@ MessageService.prototype.stopSync = function () {
         {name: 'opId', type: types_.LONG}
     ]);
 
-    // Additionally to pass the MessageService_email_stopSync_opId_TypeMismatch test
-    if (!isFinite(arguments[0])) {
-        throw new WebAPIException(WebAPIException.TYPE_MISMATCH_ERR);
-    }
-
     var self = this;
     bridge.sync({
         cmd: 'MessageService_stopSync',
