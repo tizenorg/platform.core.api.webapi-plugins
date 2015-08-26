d0e4d5f437d88b121ebbff8bc1d3a47de33beedc
Piotr Kosko <p.kosko@samsung.com>
2015-08-26 12:13:46 +0200

    [Systeminfo] Module reorganized
    [Feature] Added class SysteminfoManger for future purposes.
      Implementation of SysteminfoDeviceCapability moved to own .cc and .h files.
      Implementation of getCapabilities and getCapability methods was moved to SysteminfoManager class.
      Small fix in implementation of check*Capability methods.
    
    [Verification] TCT r37 pass rate 99.66%
    Failed test is TC issue.
    
    Change-Id: I9c34e0b1246519e8e179ba9d234accb46dfe2e49
    Signed-off-by: Piotr Kosko <p.kosko@samsung.com>

--- src/systeminfo/systeminfo_instance.cc
+++ src/systeminfo/systeminfo_instance.cc
@@ -26,6 +26,7 @@
 #include "common/task-queue.h"
 
 #include "systeminfo-utils.h"
+#include "systeminfo_device_capability.h"
 
 namespace extension {
 namespace systeminfo {
@@ -116,7 +117,7 @@ const std::string kPropertyIdCameraFlash= "CAMERA_FLASH";
   result_obj.insert(std::make_pair(str_name, picojson::value(str_value)));
 }
 
-SysteminfoInstance::SysteminfoInstance() {
+SysteminfoInstance::SysteminfoInstance() : manager_(*this) {
   LoggerD("Enter");
   using std::placeholders::_1;
   using std::placeholders::_2;
@@ -162,103 +163,12 @@ SysteminfoInstance::~SysteminfoInstance() {
 
 void SysteminfoInstance::GetCapabilities(const picojson::value& args, picojson::object& out) {
   LoggerD("Enter");
-  picojson::value result = picojson::value(picojson::object());
-  picojson::object& result_obj = result.get<picojson::object>();
-
-  bool bool_value = false;
-  int int_value = 0;
-  std::string str_value = "";
-  PlatformResult ret(ErrorCode::NO_ERROR);
-  DEFAULT_REPORT_BOOL_CAPABILITY("bluetooth", "tizen.org/feature/network.bluetooth")
-  DEFAULT_REPORT_BOOL_CAPABILITY("nfc", "tizen.org/feature/network.nfc")
-  DEFAULT_REPORT_BOOL_CAPABILITY("nfcReservedPush", "tizen.org/feature/network.nfc.reserved_push")
-  DEFAULT_REPORT_INT_CAPABILITY("multiTouchCount", "tizen.org/feature/multi_point_touch.point_count")
-  DEFAULT_REPORT_BOOL_CAPABILITY("inputKeyboard", "tizen.org/feature/input.keyboard")
-  REPORT_BOOL_CAPABILITY("inputKeyboardLayout", SystemInfoDeviceCapability::IsInputKeyboardLayout)
-  DEFAULT_REPORT_BOOL_CAPABILITY("wifi", "tizen.org/feature/network.wifi")
-  DEFAULT_REPORT_BOOL_CAPABILITY("wifiDirect", "tizen.org/feature/network.wifi.direct")
-  DEFAULT_REPORT_STRING_CAPABILITY("platformName", "tizen.org/system/platform.name")
-  DEFAULT_REPORT_STRING_CAPABILITY("platformVersion", "tizen.org/feature/platform.version")
-  DEFAULT_REPORT_STRING_CAPABILITY("webApiVersion", "tizen.org/feature/platform.web.api.version")
-  DEFAULT_REPORT_BOOL_CAPABILITY("fmRadio", "tizen.org/feature/fmradio")
-  DEFAULT_REPORT_BOOL_CAPABILITY("opengles", "tizen.org/feature/opengles")
-  DEFAULT_REPORT_BOOL_CAPABILITY("openglesVersion1_1", "tizen.org/feature/opengles.version.1_1")
-  DEFAULT_REPORT_BOOL_CAPABILITY("openglesVersion2_0", "tizen.org/feature/opengles.version.2_0")
-  REPORT_STRING_CAPABILITY("openglestextureFormat",
-                           SystemInfoDeviceCapability::GetOpenglesTextureFormat)
-  DEFAULT_REPORT_BOOL_CAPABILITY("speechRecognition", "tizen.org/feature/speech.recognition")
-  DEFAULT_REPORT_BOOL_CAPABILITY("speechSynthesis", "tizen.org/feature/speech.synthesis")
-  DEFAULT_REPORT_BOOL_CAPABILITY("accelerometer", "tizen.org/feature/sensor.accelerometer")
-  DEFAULT_REPORT_BOOL_CAPABILITY("accelerometerWakeup", "tizen.org/feature/sensor.accelerometer.wakeup")
-  DEFAULT_REPORT_BOOL_CAPABILITY("barometer", "tizen.org/feature/sensor.barometer")
-  DEFAULT_REPORT_BOOL_CAPABILITY("barometerWakeup", "tizen.org/feature/sensor.barometer.wakeup")
-  DEFAULT_REPORT_BOOL_CAPABILITY("gyroscope", "tizen.org/feature/sensor.gyroscope")
-  DEFAULT_REPORT_BOOL_CAPABILITY("gyroscopeWakeup", "tizen.org/feature/sensor.gyroscope.wakeup")
-  DEFAULT_REPORT_BOOL_CAPABILITY("camera", "tizen.org/feature/camera")
-  DEFAULT_REPORT_BOOL_CAPABILITY("cameraFront", "tizen.org/feature/camera.front")
-  DEFAULT_REPORT_BOOL_CAPABILITY("cameraFrontFlash", "tizen.org/feature/camera.front.flash")
-  DEFAULT_REPORT_BOOL_CAPABILITY("cameraBack", "tizen.org/feature/camera.back")
-  DEFAULT_REPORT_BOOL_CAPABILITY("cameraBackFlash", "tizen.org/feature/camera.back.flash")
-  DEFAULT_REPORT_BOOL_CAPABILITY("location", "tizen.org/feature/location")
-  DEFAULT_REPORT_BOOL_CAPABILITY("locationGps", "tizen.org/feature/location.gps")
-  DEFAULT_REPORT_BOOL_CAPABILITY("locationWps", "tizen.org/feature/location.wps")
-  DEFAULT_REPORT_BOOL_CAPABILITY("microphone", "tizen.org/feature/microphone")
-  DEFAULT_REPORT_BOOL_CAPABILITY("usbHost", "tizen.org/feature/usb.host")
-  DEFAULT_REPORT_BOOL_CAPABILITY("usbAccessory", "tizen.org/feature/usb.accessory")
-  DEFAULT_REPORT_BOOL_CAPABILITY("screenOutputRca", "tizen.org/feature/screen.output.rca")
-  DEFAULT_REPORT_BOOL_CAPABILITY("screenOutputHdmi", "tizen.org/feature/screen.output.hdmi")
-  DEFAULT_REPORT_BOOL_CAPABILITY("graphicsAcceleration", "tizen.org/feature/graphics.acceleration")
-  DEFAULT_REPORT_BOOL_CAPABILITY("push", "tizen.org/feature/network.push")
-  DEFAULT_REPORT_BOOL_CAPABILITY("telephony", "tizen.org/feature/network.telephony")
-  DEFAULT_REPORT_BOOL_CAPABILITY("telephonyMms", "tizen.org/feature/network.telephony.mms")
-  DEFAULT_REPORT_BOOL_CAPABILITY("telephonySms", "tizen.org/feature/network.telephony.sms")
-  REPORT_STRING_CAPABILITY("platformCoreCpuArch",
-                             SystemInfoDeviceCapability::GetPlatfomCoreCpuArch)
-  REPORT_STRING_CAPABILITY("platformCoreFpuArch",
-                             SystemInfoDeviceCapability::GetPlatfomCoreFpuArch)
-  DEFAULT_REPORT_BOOL_CAPABILITY("sipVoip", "tizen.org/feature/sip.voip")
-  DEFAULT_REPORT_BOOL_CAPABILITY("magnetometer", "tizen.org/feature/sensor.magnetometer")
-  DEFAULT_REPORT_BOOL_CAPABILITY("magnetometerWakeup", "tizen.org/feature/sensor.magnetometer.wakeup")
-  DEFAULT_REPORT_BOOL_CAPABILITY("photometer", "tizen.org/feature/sensor.photometer")
-  DEFAULT_REPORT_BOOL_CAPABILITY("photometerWakeup", "tizen.org/feature/sensor.photometer.wakeup")
-  DEFAULT_REPORT_BOOL_CAPABILITY("proximity", "tizen.org/feature/sensor.proximity")
-  DEFAULT_REPORT_BOOL_CAPABILITY("proximityWakeup", "tizen.org/feature/sensor.proximity.wakeup")
-  DEFAULT_REPORT_BOOL_CAPABILITY("tiltmeter", "tizen.org/feature/sensor.tiltmeter")
-  DEFAULT_REPORT_BOOL_CAPABILITY("tiltmeterWakeup", "tizen.org/feature/sensor.tiltmeter.wakeup")
-  DEFAULT_REPORT_BOOL_CAPABILITY("dataEncryption", "tizen.org/feature/database.encryption")
-  DEFAULT_REPORT_BOOL_CAPABILITY("autoRotation", "tizen.org/feature/screen.auto_rotation")
-  DEFAULT_REPORT_BOOL_CAPABILITY("visionImageRecognition", "tizen.org/feature/vision.image_recognition")
-  DEFAULT_REPORT_BOOL_CAPABILITY("visionQrcodeGeneration", "tizen.org/feature/vision.qrcode_generation")
-  DEFAULT_REPORT_BOOL_CAPABILITY("visionQrcodeRecognition", "tizen.org/feature/vision.qrcode_recognition")
-  DEFAULT_REPORT_BOOL_CAPABILITY("visionFaceRecognition", "tizen.org/feature/vision.face_recognition")
-  DEFAULT_REPORT_BOOL_CAPABILITY("secureElement", "tizen.org/feature/network.secure_element")
-  REPORT_STRING_CAPABILITY("profile", SystemInfoDeviceCapability::GetProfile)
-  DEFAULT_REPORT_STRING_CAPABILITY("nativeApiVersion", "tizen.org/feature/platform.native.api.version")
-  DEFAULT_REPORT_STRING_CAPABILITY("duid", "tizen.org/system/tizenid")
-  DEFAULT_REPORT_BOOL_CAPABILITY("screenSizeNormal", "tizen.org/feature/screen.size.normal")
-  DEFAULT_REPORT_BOOL_CAPABILITY("screenSize480_800", "tizen.org/feature/screen.size.normal.480.800")
-  DEFAULT_REPORT_BOOL_CAPABILITY("screenSize720_1280", "tizen.org/feature/screen.size.normal.720.1280")
-  DEFAULT_REPORT_BOOL_CAPABILITY("shellAppWidget", "tizen.org/feature/shell.appwidget")
-  DEFAULT_REPORT_BOOL_CAPABILITY("nativeOspCompatible", "tizen.org/feature/platform.native.osp_compatible")
-
-  ReportSuccess(result, out);
-  LoggerD("Success");
+  manager_.GetCapabilities(args, &out);
 }
 
 void SysteminfoInstance::GetCapability(const picojson::value& args, picojson::object& out) {
   LoggerD("Enter");
-  CHECK_EXIST(args, "key", out)
-  const std::string& key = args.get("key").get<std::string>();
-  LoggerD("Getting capability with key: %s ", key.c_str());
-
-  picojson::value result = picojson::value(picojson::object());
-  PlatformResult ret = SystemInfoDeviceCapability::GetCapability(key, result);
-  if (ret.IsSuccess()) {
-    ReportSuccess(result, out);
-    LoggerD("Success");
-  } else {
-    ReportError(ret, &out);
-  }
+  manager_.GetCapability(args, &out);
 }
 
 void SysteminfoInstance::GetPropertyValue(const picojson::value& args, picojson::object& out) {
---
+++ src/systeminfo/systeminfo_manager.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#ifndef WEBAPI_PLUGINS_SYSTEMINFO_SYSTEMINFO_MANAGER_H__
+#define WEBAPI_PLUGINS_SYSTEMINFO_SYSTEMINFO_MANAGER_H__
+
+#include "common/picojson.h"
+#include "common/platform_result.h"
+
+namespace extension {
+namespace systeminfo {
+
+class SysteminfoInstance;
+
+class SysteminfoManager {
+ public:
+  SysteminfoManager(SysteminfoInstance& instance);
+  ~SysteminfoManager();
+
+  void GetCapabilities(const picojson::value& args, picojson::object* out);
+  void GetCapability(const picojson::value& args, picojson::object* out);
+  void GetPropertyValue(const picojson::value& args, picojson::object* out);
+  void GetPropertyValueArray(const picojson::value& args, picojson::object* out);
+  void AddPropertyValueChangeListener(const picojson::value& args, picojson::object* out);
+  void RemovePropertyValueChangeListener(const picojson::value& args, picojson::object* out);
+  void GetMaxBrightness(const picojson::value& args, picojson::object* out);
+  void GetBrightness(const picojson::value& args, picojson::object* out);
+  void SetBrightness(const picojson::value& args, picojson::object* out);
+  void GetTotalMemory(const picojson::value& args, picojson::object* out);
+  void GetAvailableMemory(const picojson::value& args, picojson::object* out);
+  void GetCount(const picojson::value& args, picojson::object* out);
+
+ private:
+  SysteminfoInstance& instance_;
+
+};
+} // namespace systeminfo
+} // namespace webapi
+
+#endif // WEBAPI_PLUGINS_SYSTEMINFO_SYSTEMINFO_MANAGER_H__
