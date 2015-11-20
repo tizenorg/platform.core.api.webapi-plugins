c7e5fbcfc1e2cf5ca5d166d6aa1549423d67a2ba
Lukasz Wartalowicz <l.wartalowic@samsung.com>
2015-11-20 14:18:48 +0100

    Removed deprecated API of D-Bus and added new API of GDBus
    Change-Id: Ib47039c7d69766f21841178aafdbdaa9073601fa

--- src/power/power_platform_proxy.cc
+++ src/power/power_platform_proxy.cc
@@ -25,72 +25,80 @@ using namespace common;
 namespace extension {
 namespace power {
 
-PowerPlatformProxy::PowerPlatformProxy() :
-    dbus_op_("org.tizen.system.deviced",
-             "/Org/Tizen/System/DeviceD/Display",
-             "org.tizen.system.deviced.display")
-{
+PowerPlatformProxy::PowerPlatformProxy()
+    : gdbus_op_(common::gdbus::GDBusPowerWrapper::kDefaultBusName,
+                common::gdbus::GDBusPowerWrapper::kDefaultObjectPath) {
   LoggerD("Entered");
 }
 
-PowerPlatformProxy::~PowerPlatformProxy()
-{
-  LoggerD("Entered");
-}
+PowerPlatformProxy::~PowerPlatformProxy() { LoggerD("Entered"); }
 
-PowerPlatformProxy& PowerPlatformProxy::GetInstance()
-{
+PowerPlatformProxy& PowerPlatformProxy::GetInstance() {
   LoggerD("Entered");
   static PowerPlatformProxy instance;
   return instance;
 }
 
-common::PlatformResult PowerPlatformProxy::LockState(int* result)
-{
-  LoggerD("Entered");
-  DBusOperationArguments args;
-  args.AddArgumentString("lcddim");
-  args.AddArgumentString("staycurstate");
-  args.AddArgumentString("NULL");
-  args.AddArgumentInt32(0);
-
-  return dbus_op_.InvokeSyncGetInt("lockstate", &args, result);
+common::PlatformResult PowerPlatformProxy::LockState(int* result) {
+  LoggerD("Entered PPP LockState");
+  if (!gdbus_op_.LockState(result)) {
+    LoggerE("%s", gdbus_op_.GetLastError().c_str());
+    return PlatformResult(ErrorCode::UNKNOWN_ERR,
+                          "Failed to get reply from gdbus");
+  }
+  return PlatformResult(ErrorCode::NO_ERROR);
 }
 
-common::PlatformResult PowerPlatformProxy::UnlockState(int* result)
-{
-  LoggerD("Entered");
-  DBusOperationArguments args;
-  args.AddArgumentString("lcddim");
-  args.AddArgumentString("keeptimer");
-
-  return dbus_op_.InvokeSyncGetInt("unlockstate", &args, result);
+common::PlatformResult PowerPlatformProxy::UnlockState(int* result) {
+  LoggerD("Entered PPP UnlockState");
+  if (!gdbus_op_.UnlockState(result)) {
+    LoggerE("%s", gdbus_op_.GetLastError().c_str());
+    return PlatformResult(ErrorCode::UNKNOWN_ERR,
+                          "Failed to get reply from gdbus");
+  }
+  return PlatformResult(ErrorCode::NO_ERROR);
 }
 
-common::PlatformResult PowerPlatformProxy::SetBrightnessFromSettings(int* result)
-{
-  LoggerD("Entered");
-  return dbus_op_.InvokeSyncGetInt("ReleaseBrightness", nullptr, result);
+common::PlatformResult PowerPlatformProxy::SetBrightnessFromSettings(
+    int* result) {
+  LoggerD("Entered PPP SetBrightnessFromSettings");
+  if (!gdbus_op_.ReleaseBrightness(result)) {
+    LoggerE("%s", gdbus_op_.GetLastError().c_str());
+    return PlatformResult(ErrorCode::UNKNOWN_ERR,
+                          "Failed to get reply from gdbus");
+  }
+  return PlatformResult(ErrorCode::NO_ERROR);
 }
 
-common::PlatformResult PowerPlatformProxy::SetBrightness(int val, int* result)
-{
-  LoggerD("Entered");
-  DBusOperationArguments args;
-  args.AddArgumentInt32(val);
-
-  return dbus_op_.InvokeSyncGetInt("HoldBrightness", &args, result);
+common::PlatformResult PowerPlatformProxy::SetBrightness(int val, int* result) {
+  LoggerD("Entered PPP SetBrightness");
+  if (!gdbus_op_.HoldBrightness(val, result)) {
+    LoggerE("%s", gdbus_op_.GetLastError().c_str());
+    return PlatformResult(ErrorCode::UNKNOWN_ERR,
+                          "Failed to get reply from gdbus");
+  }
+  return PlatformResult(ErrorCode::NO_ERROR);
 }
 
 common::PlatformResult PowerPlatformProxy::GetBrightness(int* result) {
-  LoggerD("Entered");
-  return dbus_op_.InvokeSyncGetInt("CurrentBrightness", nullptr, result);
+  LoggerD("Entered PPP GetBrightness");
+  if (!gdbus_op_.CurrentBrightness(result)) {
+    LoggerE("%s", gdbus_op_.GetLastError().c_str());
+    return PlatformResult(ErrorCode::UNKNOWN_ERR,
+                          "Failed to get reply from gdbus");
+  }
+  return PlatformResult(ErrorCode::NO_ERROR);
 }
 
 common::PlatformResult PowerPlatformProxy::IsCustomBrightness(int* result) {
-  LoggerD("Entered");
-  return dbus_op_.InvokeSyncGetInt("CustomBrightness", nullptr, result);
+  LoggerD("Entered PPP IsCustomBrightness");
+  if (!gdbus_op_.CustomBrightness(result)) {
+    LoggerE("%s", gdbus_op_.GetLastError().c_str());
+    return PlatformResult(ErrorCode::UNKNOWN_ERR,
+                          "Failed to get reply from gdbus");
+  }
+  return PlatformResult(ErrorCode::NO_ERROR);
 }
 
-} // namespace power
-} // namespace extension
+}  // namespace power
+}  // namespace extension
