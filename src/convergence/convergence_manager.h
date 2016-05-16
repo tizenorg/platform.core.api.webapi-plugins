8a5fcdcb4a1382b1c1b8813df10efa525061296d
shulga <a.shulga@samsung.com>
2016-05-16 17:47:13 +0900

    Adding Convergence Web API
    1. Temporary removed all d2d-conv dependencies
    2. Uploaded first working version
    3. Quick fix: build config changed to support new d2d-conv-manager
    4. Fixing device and service indexing and delivery params to C++ layer
    5. Fixed failure, caused by using service handles after their lyfe cycle
    6. Applied new internal D2D Conv Channel,Payload API; removed temporary used glib json
    7. Supported local d2d services
    8. Fixed warnings from Tomasz Marciniak, Jun 2 (3 warnings about payload
       dictionaries are remained)
    
    Change-Id: Ib930f46f6f31a85fba924376903505ebcad823cc
    Signed-off-by: shulga <a.shulga@samsung.com>

---
+++ src/convergence/convergence_manager.h
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#ifndef WEBAPI_PLUGINS_CONVERGENCE_MANAGER_H__
+#define WEBAPI_PLUGINS_CONVERGENCE_MANAGER_H__
+
+#include <string>
+#include <unordered_map>
+#include <d2d_conv_manager.h>
+#include "common/tizen_result.h"
+
+namespace extension {
+namespace convergence {
+
+enum ServiceCommands {
+ SERVICE_START,
+ SERVICE_READ,
+ SERVICE_SEND,
+ SERVICE_STOP,
+};
+
+class ConvergenceManager {
+ public:
+   static ConvergenceManager &GetInstance(class ConvergenceInstance *owner);
+
+ public:
+  ConvergenceManager();
+  ~ConvergenceManager();
+  ConvergenceManager(const ConvergenceManager&) = delete;
+  ConvergenceManager(ConvergenceManager&&) = delete;
+  ConvergenceManager& operator=(const ConvergenceManager&) = delete;
+  ConvergenceManager& operator=(ConvergenceManager&&) = delete;
+
+ public:
+  common::TizenResult StartDiscovery(long timeout);
+  common::TizenResult StopDiscovery();
+  common::TizenResult Connect(const char *deviceId, const int serviceType,
+   const int curListenerId);
+  common::TizenResult Disconnect(const char *deviceId, const int serviceType);
+  common::TizenResult Start(const char *deviceId, const int serviceType,
+    const picojson::value &channel, const picojson::value &payload);
+  common::TizenResult Read(const char *deviceId, const int serviceType,
+    const picojson::value &channel, const picojson::value &payload);
+  common::TizenResult Send(const char *deviceId, const int serviceType,
+    const picojson::value &channel, const picojson::value &payload);
+  common::TizenResult Stop(const char *deviceId, const int serviceType,
+    const picojson::value &channel, const picojson::value &payload);
+  common::TizenResult SetListener(const char *deviceId, const int serviceType,
+   const int curListenerId);
+  common::TizenResult UnsetListener(const char *deviceId,
+   const int serviceType);
+  common::TizenResult CreateLocalService(const int serviceType,
+   const picojson::value &service_data);
+
+ private:
+  static void DiscoveryCb(conv_device_h device_handle,
+   conv_discovery_result_e result, void* user_data);
+  static void ServiceForeachCb(conv_service_h service_handle, void* user_data);
+  static void ServiceConnectedCb(conv_service_h service_handle,
+   conv_error_e error, conv_payload_h result, void* user_data);
+  static void FindServiceCb(conv_service_h service_handle, void* user_data);
+  static void ServiceListenerCb(conv_service_h service_handle,
+   conv_channel_h channel_handle,
+   conv_error_e error, conv_payload_h result, void* user_data);
+
+  common::TizenResult ExecuteServiceCommand(const ServiceCommands command,
+   const char *deviceId,
+   const int serviceType,
+   const picojson::value &channel,
+   const picojson::value &payload);
+
+ private:
+  conv_device_h getRemoteDevice(const char *deviceId) const;
+  conv_service_h getRemoteService(const conv_device_h device, const int serviceType) const;
+  conv_service_h getService(const char *deviceId, const int serviceType) const;
+  void destroyService(conv_service_h service);
+  bool isLocalDevice(const char *deviceId) const;
+
+ private:
+  class ConvergenceInstance *plg;
+  conv_h conv_;
+  mutable std::unordered_map<std::string, conv_device_h> devices;
+  mutable std::unordered_map<int, conv_service_h> __localServices;
+};
+
+
+} // namespace convergence
+} // namespace extension
+
+#endif // WEBAPI_PLUGINS_CONVERGENCE_MANAGER_H__
